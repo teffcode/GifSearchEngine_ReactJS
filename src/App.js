3eaa776982d9139afedc7c727928a36534ba8376
teffcode <tefy.1@live.com>
2018-05-14 20:34:57 -0500

    Gif Item and List components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 import Search from './components/Search/Search';
+import GifList from './components/GifList/GifList';
 
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+        gifs: [
+            {
+                id: 1,
+                url: 'https://png.icons8.com/nolan/2x/gif.png'
+            },
+            {
+                id: 2,
+                url: 'https://png.icons8.com/nolan/2x/gif.png'
+            },
+            {
+                id: 3,
+                url: 'https://png.icons8.com/nolan/2x/gif.png'
+            }
+        ]
+    }
+  }
+
   handleTermChange(term) {
     console.log(term);
   }
@@ -13,6 +35,7 @@ class App extends React.Component {
     return (
       <div>
         <Search onTermChange={this.handleTermChange} />
+        <GifList gifs={this.state.gifs} />
       </div>
     );
   }
