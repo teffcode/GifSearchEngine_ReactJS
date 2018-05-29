2e054e3eced5bcae85d01c60f1c5f50ec9bb5db2
teffcode <tefy.1@live.com>
2018-05-28 20:11:16 -0500

    Gifs reducer - fake data

--- src/reducers/gifs.jsx
+++ src/reducers/gifs.jsx
@@ -1,12 +1,30 @@
-function gifs(state, action) {
-    switch(action.type) {
-        case OPEN_MODAL:
-            return {
-                ...state,
-                modalIsOpen: true,
-                selectedGif: action.gif.selectedGif
-            };
-        default:
-            return state;
-    }
+export default function() {
+    return [
+        {
+            id: 1,
+            url: 'https://png.icons8.com/cotton/2x/confectionery.png'
+        },
+        {
+            id: 2,
+            url: 'https://png.icons8.com/cotton/2x/confectionery.png'
+        },
+        {
+            id: 3,
+            url: 'https://png.icons8.com/cotton/2x/confectionery.png'
+        }
+    ]
 }
+
+
+// function gifs(state, action) {
+//     switch(action.type) {
+//         case OPEN_MODAL:
+//             return {
+//                 ...state,
+//                 modalIsOpen: true,
+//                 selectedGif: action.gif.selectedGif
+//             };
+//         default:
+//             return state;
+//     }
+// }
